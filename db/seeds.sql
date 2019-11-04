acb8d8fef2e7dd3c38f5ac06ec513c18ed55276a
Shital Patel <shital732@yahoo.com>
2019-11-04 09:49:03 -0500

    add devored buton

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken bacon ');
+INSERT INTO burgers (burger_name) VALUES ('Ham and cheese');
+INSERT INTO burgers (burger_name) VALUES ('Turkey bacon');
