5ada13a15866dc6a9d42d9f9c665d83f73c2803e
Shital Patel <shital732@yahoo.com>
2019-11-05 18:55:40 -0500

    final work

--- config/connection.js
+++ config/connection.js
@@ -10,12 +10,12 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: "root",
     password: "#mar1996", // add your local password here.
-    database: "burger_db" // add your db name here
+    database: "burgers_db" // add your db name here
   });
-  connection.connect(function(err){
-    console.log("err",err)
-    console.log("Connected as ID: " + connection.threadId);
-  })
+  // connection.connect(function(err){
+  //   console.log("err",err)
+  //   console.log("Connected as ID: " + connection.threadId);
+  // })
 }
 
 
